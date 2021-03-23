a7b3f676fbf1c5c4a55b3edf18b416e5a196b6ae
Sasha Hulitskyi <alexgulitskyy3@gmail.com>
2021-03-23 17:32:21 +0200

    added ability to specify target of widget

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,4 +12,5 @@ module.exports = {
     'no-console': 'off',
   },
   plugins: ['prettier'],
+  parser: '@babel/eslint-parser',
 };
--- src/AuthPM.js
+++ src/AuthPM.js
@@ -1,6 +1,34 @@
 class AuthPM {
-  constructor() {
-    console.log('init');
+  #appId;
+
+  #target;
+
+  constructor(appId, target) {
+    this.#appId = appId;
+
+    this.#setTarget(target);
+    this.#getAppList();
+  }
+
+  #setTarget(target) {
+    if (AuthPM.#isDomElement(target)) {
+      this.#target = target;
+      return;
+    }
+
+    const possibleTarget = document.querySelector(target);
+
+    if (AuthPM.#isDomElement(possibleTarget)) {
+      this.#target = possibleTarget;
+    }
+  }
+
+  #getAppList() {
+    console.log(this.#appId);
+  }
+
+  static #isDomElement(obj) {
+    return obj instanceof Element;
   }
 }
 
